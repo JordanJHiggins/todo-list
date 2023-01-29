95a4f13f880b711cf0c883e3d59e0eea6fc889f0
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-01-29 14:17:50 -0400

    add Ui prototyping

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import Project from './project';
 import Task from './task';
+import TodoList from './todoList';
+import Ui from './UI';
+import './style.css';
 
 // Logic Testing
+
 const project1 = new Project('projectOne');
+const project2 = new Project('Work');
+
+// app init / default
+const todoListOne = new TodoList();
+
+todoListOne.addProject(project1);
+todoListOne.addProject(project2);
 
 const task1 = new Task('taskOne', 'code a todo app', '06/12/2023', 'high');
 const task2 = new Task('taskTwo', 'eat a tasty salad', '07/26/2024', 'low');
 
-const data = { title: 'bingbong', priority: 'extreme!!' };
+// const data = { title: 'bingbong', priority: 'extreme!!' };
+
 project1.addTask(task1);
 project1.addTask(task2);
 
-project1.updateTask(task1.id, data);
-project1.updateTask(task2.id, data);
-
 console.log(project1);
+console.log(todoListOne);
+
+// UI Testing
+const newUi = new Ui();
+
+newUi.createProjectCard(project1.title);
+newUi.createProjectCard(project2.title);
+
+// const myDate = document.getElementById('date');
+// myDate.addEventListener('change', () => {
+//   console.log(myDate.value);
+// });
