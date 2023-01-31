2c877603f6bb1013dfd3ff3019b81e3309aa84d7
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-01-31 18:30:00 -0400

    add new task form to modal

--- src/UI.js
+++ src/UI.js
@@ -31,21 +31,37 @@ export default class Ui {
     mainContent.id = 'main-content';
     mainContent.innerHTML = `<h2 id="inbox-title">Inbox</h2>
     <button class="task-button">Add task </button>
-    <dialog class="modal"> <input type="text"></input></dialog> 
+    <dialog class="modal"> 
+    <form>
+    <label for="title">Title</label><br>
+    <input type="text" id="title-input"><br>
+    <label for="desc">Description</label><br>
+    <input type="text" id="desc-input"><br>
+    <label for="due-date">Due Date</label><br>
+    <input type="date id="date-input"><br>
+    <label for="priority"> Priority</><br>
+    <input type="radio" id="priority-input"><br>
+    <button class="close-modal">Close Modal</button>
+    </form>
+    </dialog> 
     `;
 
     gridWrapper.appendChild(mainContent);
   }
 
   // Event Listeners
-
   initTaskButton() {
+    const modal = document.querySelector('.modal');
     const taskButton = document.querySelector('.task-button');
+    const closeModal = document.querySelector('.close-modal');
     // separate opening modal to its own function?
     taskButton.addEventListener('click', () => {
-      const modal = document.querySelector('.modal');
       console.log('issamodal');
       modal.showModal();
+    });
+
+    closeModal.addEventListener('click', () => {
+      modal.close();
     });
   }
 }
