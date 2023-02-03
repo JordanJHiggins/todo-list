4e68fe10cb6daf3d311947d19dac5f1900850710
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-02-03 12:05:00 -0400

    fix add task not showing form for all projects

--- src/UI.js
+++ src/UI.js
@@ -4,20 +4,40 @@ import Project from './project';
 
 export default class Ui {
   // Create Elements
+
+  // creates HTML object from JS code.
+  elementFromHtml(html) {
+    const template = document.createElement('template');
+
+    template.innerHTML = html.trim();
+
+    return template.content.firstElementChild;
+  }
+
   createProjectCard(project) {
     const mainContent = document.getElementById('main-content');
     const projectCard = document.createElement('div');
-    projectCard.classList.add('project-card');
-    projectCard.innerHTML = `<h2>${project.title}</h2>`;
 
+    const projectCardContent = this.elementFromHtml(`
+    <div>
+      <h2>${project.title}</h2>
+      <button class="task-button">Add task</button>
+    </div>`);
+
+    projectCard.classList.add('project-card');
+
+    projectCard.appendChild(projectCardContent);
     mainContent.appendChild(projectCard);
+    this.createTaskForm();
+    this.initTaskButton();
   }
 
   createTaskCard(task) {
-    const projectCard = document.getElementsByClassName('project-card')[0];
+    const projectCard = document.getElementsByClassName('project-card');
     const taskCard = document.createElement('div');
 
-    taskCard.innerHTML = `<h5>${task.title}</h5>
+    taskCard.innerHTML = `
+    <h5>${task.title}</h5>
     <h5>${task.desc}</h5>
     <h5>${task.dueDate}</h5>
     <h5>${task.priority}</h5>
@@ -40,11 +60,9 @@ export default class Ui {
 
   createTaskForm() {
     const gridWrapper = document.getElementById('grid-wrapper');
-    const mainContent = document.createElement('div');
-    mainContent.id = 'main-content';
-    mainContent.innerHTML = `
-    <button class="task-button">Add task </button>
-    <dialog id="task-modal"> 
+    const mainContent = document.getElementById('main-content');
+
+    const formContent = this.elementFromHtml(`<dialog id="task-modal"> 
     <form method="dialog" id="task-form">
     <label for="title">Title</label><br>
     <input type="text" id="title-input"><br>
@@ -57,62 +75,38 @@ export default class Ui {
     <button class="close-modal">Close Modal</button>
     <button class="task-submit" type="submit">Submit</button>
     </form>
-    </dialog> 
-    `;
+    </dialog>`);
+
+    mainContent.appendChild(formContent);
     gridWrapper.appendChild(mainContent);
   }
 
   loadHome() {
     const newTodoList = new TodoList();
     const newInbox = new Project('Inbox');
+
     newTodoList.addProject(newInbox);
-    this.createTaskForm();
     this.createProjectForm();
-    this.createProjectCard(newTodoList);
+    this.createProjectCard(newInbox);
+
     this.createNewProject(newTodoList);
+    this.createTaskForm();
+
+    this.initProjectButton();
+    this.initTaskButton();
   }
 
   // Event Listeners
-  createNewProject(todoList) {
-    const projectForm = document.getElementById('project-form');
-    let newProject = new Project();
-
-    projectForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const projectTitle = document.getElementById('project-title').value;
-
-      newProject = new Project(projectTitle);
-      this.createProjectCard(newProject);
-      todoList.addProject(newProject);
-
-      console.log(newProject);
-      console.log(todoList);
-    });
-  }
-
-  listenAddTask() {
-    const taskForm = document.getElementById('task-form');
-    // need to get values after submisson
-    taskForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const taskTitle = document.getElementById('title-input').value;
-      const taskDesc = document.getElementById('desc-input').value;
-      const taskDueDate = document.getElementById('date-input').value;
-      const taskPriority = document.getElementById('priority-input').value;
-      this.createNewTask(taskTitle, taskDesc, taskDueDate, taskPriority);
-    });
-
-    this.createNewTask(taskTitle, taskDesc, taskDueDate, taskPriority);
-  }
-
   initTaskButton() {
     const taskModal = document.querySelector('#task-modal');
-    const taskButton = document.querySelector('.task-button');
+    const taskButton = document.querySelectorAll('.task-button');
     const closeModal = document.querySelector('.close-modal');
     // separate opening modal to its own function?
-    taskButton.addEventListener('click', () => {
-      taskModal.showModal();
-    });
+    taskButton.forEach((button) =>
+      button.addEventListener('click', () => {
+        taskModal.showModal();
+      })
+    );
 
     closeModal.addEventListener('click', () => {
       taskModal.close();
@@ -130,6 +124,41 @@ export default class Ui {
 
     closeModal.addEventListener('click', () => {
       projectModal.close();
+    });
+  }
+
+  createNewProject(todoList) {
+    const projectForm = document.getElementById('project-form');
+    let newProject = new Project();
+
+    projectForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const projectTitle = document.getElementById('project-title').value;
+
+      newProject = new Project(projectTitle);
+      this.createProjectCard(newProject);
+      todoList.addProject(newProject);
+
+      console.log(newProject);
+      console.log(todoList);
+    });
+  }
+
+  createNewTask() {
+    const taskForm = document.getElementById('task-form');
+    let newTask = new Task();
+
+    taskForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const taskTitle = document.getElementById('title-input').value;
+      const taskDesc = document.getElementById('desc-input').value;
+      const taskDueDate = document.getElementById('date-input').value;
+      const taskPriority = document.getElementById('priority-input').value;
+
+      newTask = new Task(taskTitle, taskDesc, taskDueDate, taskPriority);
+
+      this.createTaskCard(newTask);
+      // project.addTask(newTask);
     });
   }
 }
